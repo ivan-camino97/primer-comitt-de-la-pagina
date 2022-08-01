4e79b7cc3a6933b17dda7f2356e08ad6b511330d
ivan camino <caminoivan904@gmail.com>
2022-08-01 12:55:30 -0300

    1/8

--- js/index.js
+++ js/index.js
@@ -65,7 +65,7 @@ for(let i = 0; i <= 10; i++)
     console.log(i + 'x' + numero + '=' + resultado);
 }*/
 
-/*ejercico switch*/
+/*ejercico switch
 let facturas = 960
 let budines = 550
 let pan = 340
@@ -106,4 +106,129 @@ do {
 } while (eleccionUsuario != "facturas" && eleccionUsuario != "pan" && eleccionUsuario != "budines" && eleccionUsuario != "budines y facturas" && eleccionUsuario != "budines y pan" && eleccionUsuario != "facturas y pan")
 
 }
-carrito()
+carrito()*/
+
+/* objetos */
+/*  const facturas = {
+    unidad: 90,
+    docena: 1080,
+    media: 540,
+
+
+}
+
+const pan = {
+    tipo: 'frances',
+    kg: 400,
+    medio: 200
+
+}
+
+const pan2 = {
+    tipo: 'manteca',
+    kg: 580,
+    medio: 240
+}
+
+console.log(pan2);
+/*agrega otro elemento*/
+// pan.rayadoKg = 400
+/*cambia valor de elemento*/
+// pan2.kg = 600
+// console.log(pan2);
+/*funcion constructora*/
+/*function Panes(tipo, unidad, docena){
+    this.tipo = tipo;
+    this.unidad = unidad;
+    this.docena = docena;
+    this.mostrarunidad = () => {console.log('la unidad vale:' + this.unidad)}/*metodos*/
+/*}
+const panes1 = new Panes("pebetes", 90, 1080);
+const panes2 = new Panes("pan de molde salvado", 500);
+console.log(panes1);
+console.log(panes2);
+panes2.mostrarunidad()/*metodos*/
+/*for in*/
+/*for(const variablePan in panes1){
+    console.log(panes1[variablePan])
+}
+/*clases:funciona igual que la sintaxis con funcion constructora*/
+/*class pan3{
+    constructor(tipo, unidad){
+        this.tipo = tipo;
+    this.unidad = unidad;
+
+    }
+    mostrarunidad(){
+        console.log('la unidad vale:' + this.unidad);
+    }
+} */
+
+let carrito = []
+class Panes{
+    constructor(tipo, kg, unidad,){
+    this.tipo = tipo
+    this.kg = kg
+    this.unidad = unidad
+    }
+}
+const Panes1 = new Panes('manteca', 580, 35)
+const Panes2 = new Panes('frances', 400, 70,)
+const panes3 = new Panes('pebetes', 0, 90 )
+const panes4 = new Panes('molde salvado', 580, 580)
+const panes5 = new Panes('campo', 400, 350)
+const panes6 = new Panes('negros salvado', 580, 35)
+
+const eleccionProducto = () => {
+
+const eleccionUsuario = prompt('elegi el tipo de pan que desees')
+
+switch (eleccionUsuario) {
+    case 'manteca':
+        console.log('elegiste pan de manteca')
+        carrito.push(Panes1)
+        break;
+
+        case 'frances':
+        console.log('elegiste pan frances')
+        carrito.push(Panes2)
+            break;
+
+            case 'pebetes':
+        console.log('elegiste pebetes')
+        carrito.push(panes3)
+        break;
+
+        case 'molde salvado':
+        console.log('elegiste pan de molde de salvado')
+        carrito.push(panes4)
+        break;
+
+        case 'campo':
+        console.log('elegiste pan de campo')
+        carrito.push(panes5)
+        break;
+
+        case 'negro salvado':
+            console.log('elegiste pan negro de salvado')
+            carrito.push(panes6)
+        break;
+    default:
+        console.log('elegi un tipo de pan correcto')
+        break;
+}
+if (confirm('desea agregar otro producto?')) {
+    eleccionProducto()
+}
+}
+eleccionProducto()
+
+const totalCarrito = () => {
+   let sumaTotal = 0
+   for(const producto of carrito){
+       sumaTotal += producto.kg || producto.unidad
+   }
+   return sumaTotal
+}
+
+alert('gracias por su compra,el total es de $' + totalCarrito())
