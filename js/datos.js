d46e31a58b6c0ac18cf988bdc6a3502b3756439b
ivan camino <caminoivan904@gmail.com>
2022-09-18 19:03:25 -0300

    18/9

--- js/datos.js
+++ js/datos.js
@@ -1,13 +1,14 @@
 class Alfajores{
-    constructor(tipo, precio, imagen, id, descripcion){
+    constructor(tipo, precio, imagen, id, descripcion, cantidad){
     this.tipo = tipo
     this.precio = precio
     this.imagen = imagen
     this.id = id
     this.descripcion = descripcion
+    this.cantidad = cantidad
     }
 }
-const blanco = new Alfajores('blanco con dulce de leche', 200, '../images/dulcedeleche.jpg', 001, 'alfajor blanco con dulce de leche')
-const chocolate = new Alfajores('chocolate', 200, '../images/alfchoco.jpg', 002, 'alfajor de chocolate con dulce de leche')
-const maizena = new Alfajores('maizena', 200, '../images/mmaizena.jpg', 003, 'alfajor de maizena')
-const santafesino = new Alfajores('santafesino', 200, '../images/santafesino.jpg', 004, 'alfajor santafesino tipo roggel')
+const blanco = new Alfajores('blanco con dulce de leche', 200, '../images/dulcedeleche.jpg', 001, 'alfajor blanco con dulce de leche', 1)
+const chocolate = new Alfajores('chocolate', 180, '../images/alfchoco.jpg', 002, 'alfajor de chocolate con dulce de leche', 1)
+const maizena = new Alfajores('maizena', 220, '../images/mmaizena.jpg', 003, 'alfajor de maizena', 1)
+const santafesino = new Alfajores('santafesino', 250, '../images/santafesino.jpg', 004, 'alfajor santafesino tipo roggel', 1)
--- js/index.js
+++ js/index.js
@@ -7,6 +7,10 @@ const allalfajor = [blanco, chocolate, maizena, santafesino]
 
 /*selectores*/
 const contenedoralfajor = document.querySelector('#contenedoralfajor')
+const botontabla = document.querySelector('.botontabla')
+const tablacarrito = document.querySelector('tbody')
+const botonvaciar = document.querySelector ('.botonvaciar')
+const total = document.querySelector('.total')
 
 const casilleroname = document.querySelector('.casilleroname')
 const casilleroprecio = document.querySelector('.casilleroprecio')
@@ -24,10 +28,18 @@ const casilleroname3 = document.querySelector('.casilleroname3')
 const casilleroprecio3 = document.querySelector('.casilleroprecio3')
 const casillerocantidad3 = document.querySelector('.casillerocantidad3')
 
+let cantidad = 0
 
-/*funciones*/
+document.addEventListener('DOMcontentloaded', () => {
+    if(localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.setItem('carrito'))
+
+        actualizarcarrito()
+    }
+})
+/*funcion de renderizado*/
 const renderizarcarrito = () => {
-    allalfajor.forEach(({tipo, precio, imagen, id, descripcion}) => {
+    allalfajor.forEach(({tipo, precio, imagen, id, descripcion, cantidad}) => {
 
         const alfajorarticle = document.createElement('article')
         alfajorarticle.classList.add('contenedor10')
@@ -40,53 +52,73 @@ const renderizarcarrito = () => {
             <article>
                <p>precio: ${precio}</p>
                <span>${descripcion}</span>
-               <button data-id="${id}" class="btn">comprar</button>
+               <button data-id="${id}" class="btn">agregar al carrito <i class="fas fa-shopping-cart"></i></button>
             </article>
          </article>   
         `
           
         contenedoralfajor.append(alfajorarticle)
 
-    })
+        })
 }
 renderizarcarrito()
+//funcion para actualizar el carrito y agregar los alfajores
+const actualizarcarrito = () => {
+
+    tablacarrito.innerHTML = ''
+
+    carrito.forEach((alfajor) =>{
+        const tr = document.createElement('tr')
+        tr.className = ('tr')
+        tr.innerHTML = `
+           <td class="td">${alfajor.tipo}</td>
+           <td class="td">${alfajor.precio}</td>
+           <td class="td">${alfajor.cantidad}</td>
+        `
+tablacarrito.appendChild(tr)
+localStorage.setItem('carrito', JSON.stringify(carrito))
+    })
+    total.innerText = carrito.reduce((acc,alfajor) => acc + alfajor.precio, 0)
+}
 
 const buttonCarrito = document.querySelectorAll('.btn')
     console.log(buttonCarrito);
-
+//funcion para agregegar id a cada boton y seleccionar alfajores
 const agregaralfajores = (e) => {
+
+    const existe = carrito.some (alfajor => alfajor.id === alfajorseleccionado)
+
+    if(existe) {
+        const alf = carrito.map (alf => {
+            if (alfajor.id === alfajorseleccionado)
+            alfajoridseleccionado.cantidad++
+        })
+        }
+
     const alfajoridseleccionado = e.target.getAttribute('data-id')
     const alfajorseleccionado = allalfajor.find((Alfajor) => Alfajor.id == alfajoridseleccionado)
     const alblanco = alfajoridseleccionado == 001 && console.log('blanco')
     const alnegro = alfajoridseleccionado == 002 && console.log('negro')
     carrito.push(alfajorseleccionado)
     console.log(carrito);
+    actualizarcarrito()
     fetch('https://pokeapi.co/api/v2/pokemon/25')
 .then((respuesta) => respuesta.json())
 .then((respuestaapi) => {
     const alfajorpokemon =  'el alfajor N°' + alfajoridseleccionado + 'le gusta a' + respuestaapi.name
     carrito.push(alfajorpokemon)
-
-    if (alfajoridseleccionado == 001){
-        casilleroname.textContent = blanco.tipo
-        casilleroprecio.textContent = blanco.precio
-    }else if (alfajoridseleccionado == 002){
-        casilleroname1.textContent = chocolate.tipo
-        casilleroprecio1.textContent = chocolate.precio
-    }else if (alfajoridseleccionado == 003){
-        casilleroname2.textContent = maizena.tipo
-        casilleroprecio2.textContent = maizena.precio
-    }else if (alfajoridseleccionado == 004){
-        casilleroname3.textContent = santafesino.tipo
-        casilleroprecio3.textContent = santafesino.precio
-    }
-
 })
-    
 }
+
+
 //eventlisteners
 buttonCarrito.forEach((button) => {
     button.addEventListener('click', agregaralfajores)
-})
+    })
+
+    botonvaciar.addEventListener('click', () => {
+        carrito.length = 0
+        actualizarcarrito()
+    })
 
 //ejecuciones
