d77e069ea74349e44b014562ec9837e3dd7468bb
ivan camino <caminoivan904@gmail.com>
2022-10-11 20:54:07 -0300

    carrito

--- js/datos.js
+++ js/datos.js
@@ -1,14 +1,6 @@
-class Alfajores{
-    constructor(tipo, precio, imagen, id, descripcion, cantidad){
-    this.tipo = tipo
-    this.precio = precio
-    this.imagen = imagen
-    this.id = id
-    this.descripcion = descripcion
-    this.cantidad = cantidad
-    }
-}
-const blanco = new Alfajores('blanco con dulce de leche', 200, '../images/dulcedeleche.jpg', 001, 'alfajor blanco con dulce de leche', 1)
-const chocolate = new Alfajores('chocolate', 180, '../images/alfchoco.jpg', 002, 'alfajor de chocolate con dulce de leche', 1)
-const maizena = new Alfajores('maizena', 220, '../images/mmaizena.jpg', 003, 'alfajor de maizena', 1)
-const santafesino = new Alfajores('santafesino', 250, '../images/santafesino.jpg', 004, 'alfajor santafesino tipo roggel', 1)
+let stockProductos = [
+    {tipo: 'blanco con dulce de leche', precio: 200, img:'../images/dulcedeleche.jpg', id: 001, descripcion: 'alfajor con azucar impalpable y dulce de leche', cantidad: 1 },
+    {tipo: 'chocolate', precio: 180, img: '../images/alfchoco.jpg', id: 002, descripcion: 'alfajor con cobertura de chocolate', cantidad: 1 },
+    {tipo: 'maizena', precio: 220, img: '../images/mmaizena.jpg', id: 003, descripcion: 'alfajor de maizena', cantidad: 1 },
+    {tipo: 'santafesino', precio: 250, img: '../images/santafesino.jpg', id: 004, descripcion: 'alfajor tipo rogel', cantidad: 1 }
+]
